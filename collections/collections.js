4353fa995a6042de7cc55106f25494d1fda6d55d
Daniel Opatich <daniel.opatich@gmail.com>
2015-11-10 23:15:32 -0600

    almost final

--- collections/collections.js
+++
@@ -1,6 +0,0 @@
-// COLLECTIONS //
-App.Collection.ContactCollection = Backbone.Collection.extend({
-  url: 'https://tiny-starburst.herokuapp.com/collections',
-  model: App.Models.Contact
-});
-// END COLLECTIONS //
--- main.js
+++ main.js
@@ -4,21 +4,20 @@ App.View={};
 App.Collection={};
 
 
-
 // ROUTER //
-App.Router = Backbone.Router.extend({
+var Router = Backbone.Router.extend({
   routes: {
     '': 'home',
     'contacts': 'contacts'
   },
   home: function(){
-    var newHome = new App.View.Home();
+    var newHome = new HomeView();
     $('#main').append(view.render().el);
   },
 
   contacts: function(){
-    var collection = new App.Collection.ContactCollection();
-    var newContacts = new App.View.ContactsCollection({
+    var collection = new ContactColl();
+    var newContacts = new HomeView({
       collection: collection
     });
       collection.fetch({
@@ -30,6 +29,73 @@ App.Router = Backbone.Router.extend({
 });
 
 // END ROUTER //
+
+// MODELS //
+var ContactModel = Backbone.Model.extend({
+  url: 'https://tiny-starburst.herokuapp.com/collections',
+  default: {
+    first   : '',
+    last    : '',
+    phone   : '',
+    twitter : '',
+    github  : '',
+  },
+});
+// END MODELS //
+
+// COLLECTIONS //
+var ContactColl = Backbone.Collection.extend({
+  url: 'https://tiny-starburst.herokuapp.com/collections',
+  model: App.Models.Contact
+});
+// END COLLECTIONS //
+
+// VIEWS //
+
+var HomePage = Backbone.View.extend({
+  initialize:function(options){
+    $(".submitBtn").on("click", this.send);
+  },
+  template: _.template($('#inputTemplate').html()),
+  events: {
+    'click .submitBtn': 'handleClick'
+  },
+
+  send: function(){
+    var first = $('.first').val();
+    var last = $('.last').val();
+    var phone = $('.phone').val();
+    var twitter = $('.twitter').val();
+    var github = $('.github').val();
+
+    var newContact = new ContactColl({
+      first: first,
+      last: last,
+      phone: phone,
+      twitter: twitter,
+      github: github,
+    });
+
+  }
+  newContact.save();
+},
+  render: function(){
+    this.$el.html(this.template());
+  },
+  handleClick: function(event){
+    console.log('User submitted contact info');
+    event.preventDefault();
+    this.send();
+  },
+});
+
+var ContactsView = Backbone.View.extend({
+  render: function(){
+    var template = _.template($('#contactTemplate').html(), {user: this.collection.toJSON()});
+    this.$el.html(template);
+  },
+});
+// END VIEWS //
 
 
 $('document').ready(function() {
